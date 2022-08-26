6fc5b81e368813af18975f825c6375ae40e7cb93
dimok8a <dimok4a@mail.ru>
2022-08-26 10:22:41 +0400

    ...

--- index.js
+++ index.js
@@ -1,5 +1,11 @@
-console.log("It works")
-console.log("It works")
-console.log("It works")
-console.log("It works")
-console.log("It works")
+const express = require("express")
+const path = require("path");
+
+const app = express();
+app.use(express.static(path.resolve(__dirname, 'public')));
+const PORT = 5000
+
+app.get("/*", (req, res)=> {
+    res.sendFile(path.resolve(__dirname, 'client', 'index.html'));
+})
+app.listen(PORT, () => console.log(`server has been started on port ${PORT}`));
